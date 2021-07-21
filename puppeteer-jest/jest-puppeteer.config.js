343969a97b18a89957cf23f076f98506f6ef98dd
sourav-kundu <66624530+sourav-kundu@users.noreply.github.com>
2021-07-21 11:36:55 +0530

    indent fix

--- puppeteer-jest/jest-puppeteer.config.js
+++ puppeteer-jest/jest-puppeteer.config.js
@@ -1,34 +1,34 @@
 const caps_chrome = {
-    'browser': 'chrome',
-    'browser_version': 'latest',
-    'os': 'osx',
-    'os_version': 'big sur',
-    'name': 'Puppeteer-jest test on Chrome',
-    'build': 'puppeteer-jest-build-2',
-    'browserstack.username': process.env.BROWSERSTACK_USERNAME || 'YOUR_USERNAME',
-    'browserstack.accessKey': process.env.BROWSERSTACK_ACCESS_KEY || 'YOUR_ACCESS_KEY'
+  'browser': 'chrome',
+  'browser_version': 'latest',
+  'os': 'osx',
+  'os_version': 'big sur',
+  'name': 'Puppeteer-jest test on Chrome',
+  'build': 'puppeteer-jest-build-2',
+  'browserstack.username': process.env.BROWSERSTACK_USERNAME || 'YOUR_USERNAME',
+  'browserstack.accessKey': process.env.BROWSERSTACK_ACCESS_KEY || 'YOUR_ACCESS_KEY'
 };
 
 const caps_firefox = {
-    'browser': 'firefox',
-    'browser_version': 'latest',
-    'os': 'osx',
-    'os_version': 'big sur',
-    'name': 'Puppeteer-jest test on Firefox',
-    'build': 'puppeteer-jest-build-2',
-    'browserstack.username': process.env.BROWSERSTACK_USERNAME || 'YOUR_USERNAME',
-    'browserstack.accessKey': process.env.BROWSERSTACK_ACCESS_KEY || 'YOUR_ACCESS_KEY'
+  'browser': 'firefox',
+  'browser_version': 'latest',
+  'os': 'osx',
+  'os_version': 'big sur',
+  'name': 'Puppeteer-jest test on Firefox',
+  'build': 'puppeteer-jest-build-2',
+  'browserstack.username': process.env.BROWSERSTACK_USERNAME || 'YOUR_USERNAME',
+  'browserstack.accessKey': process.env.BROWSERSTACK_ACCESS_KEY || 'YOUR_ACCESS_KEY'
 };
 
 const caps_edge = {
-    'browser': 'edge',
-    'browser_version': 'latest',
-    'os': 'osx',
-    'os_version': 'big sur',
-    'name': 'Puppeteer-jest test on Edge',
-    'build': 'puppeteer-jest-build-2',
-    'browserstack.username': process.env.BROWSERSTACK_USERNAME || 'YOUR_USERNAME',
-    'browserstack.accessKey': process.env.BROWSERSTACK_ACCESS_KEY || 'YOUR_ACCESS_KEY'
+  'browser': 'edge',
+  'browser_version': 'latest',
+  'os': 'osx',
+  'os_version': 'big sur',
+  'name': 'Puppeteer-jest test on Edge',
+  'build': 'puppeteer-jest-build-2',
+  'browserstack.username': process.env.BROWSERSTACK_USERNAME || 'YOUR_USERNAME',
+  'browserstack.accessKey': process.env.BROWSERSTACK_ACCESS_KEY || 'YOUR_ACCESS_KEY'
 };
 
 module.exports = {
