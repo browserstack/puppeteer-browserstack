91078d2fef796c4aa486244cf6503c6085af90ab
sourav-kundu <66624530+sourav-kundu@users.noreply.github.com>
2021-12-07 14:17:31 +0530

    changed google.comn to duckduckgo

--- first_test.js
+++ first_test.js
@@ -15,22 +15,23 @@ const expect = require('chai').expect;
     };
     const browser = await puppeteer.connect({
     browserWSEndpoint:
-    `wss://cdp.browserstack.com/puppeteer?caps=${encodeURIComponent(JSON.stringify(caps))}`,
+    `wss://cdp.browserstack.com?caps=${encodeURIComponent(JSON.stringify(caps))}`,
     });
 
     /* 
     *  BrowserStack specific code ends here
     */
     const page = await browser.newPage();
-    await page.goto('https://www.google.com/ncr');
-    const element = await page.$('[aria-label="Search"]');
+
+    await page.goto('https://www.duckduckgo.com');
+    const element = await page.$('[name="q"]');
     await element.click();
     await element.type('BrowserStack');
     await element.press('Enter');
     const title = await page.title('');
     console.log(title);
     try {
-        expect(title).to.equal("BrowserStack - Google Search", 'Expected page title is incorrect!');
+        expect(title).toEqual( 'BrowserStack at DuckDuckGo', 'Expected page title is incorrect!');
         // following line of code is responsible for marking the status of the test on BrowserStack as 'passed'. You can use this code in your after hook after each test
         await page.evaluate(_ => {}, `browserstack_executor: ${JSON.stringify({action: 'setSessionStatus',arguments: {status: 'passed',reason: 'Title matched'}})}`);
     } catch {
