236fc713a7730543d1e17edff7074b72ec589e94
Aditya Samantaray <aditya.sam@browserstack.com>
2022-04-05 12:50:56 +0530

    chore: use duckduckgo instead of google search

--- puppeteer-jest/duckduckgo.test.js
+++ puppeteer-jest/duckduckgo.test.js
@@ -2,17 +2,17 @@
 
 const { Browser } = require("puppeteer");
 
-describe("Google", () => {
+describe("DuckDuckGo", () => {
   beforeAll(async () => {
-    await page.goto('https://google.com')
+    await page.goto('https://www.duckduckgo.com')
   })
-  it('title should match BrowserStack - Google Search', async () => {
-    const element = await page.$('[aria-label="Search"]');
+  it('title should match BrowserStack at DuckDuckGo', async () => {
+    const element = await page.$('[name="q"]');
     await element.click();
     await element.type('BrowserStack');
-    await element.press('Enter');
+    await Promise.all([element.press('Enter'), page.waitForNavigation()]);
     try {
-      expect(await page.title()).toBe('BrowserStack - Google Search');
+      expect(await page.title()).toBe('BrowserStack at DuckDuckGo');
       await page.evaluate(_ => {}, `browserstack_executor: ${JSON.stringify({action: 'setSessionStatus',arguments: {status: 'passed',reason: 'Test assertion passed'}})}`);
     } catch {
       await page.evaluate(_ => {}, `browserstack_executor: ${JSON.stringify({action: 'setSessionStatus',arguments: {status: 'failed',reason: 'Test assertion failed'}})}`);
