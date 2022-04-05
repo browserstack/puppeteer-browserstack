4efe8de65c57e2892186bdd21587afc15f50fc0a
Aditya Samantaray <aditya.sam@browserstack.com>
2022-04-05 16:57:09 +0530

    chore: add comments for syncing with docs

--- duckduckgo_search.js
+++ duckduckgo_search.js
@@ -4,22 +4,23 @@ const puppeteer = require('puppeteer');
 const expect = require('chai').expect;
 (async () => {
     const caps = {
-        'browser': 'chrome',
-        'browser_version': 'latest',
+        'browser': 'chrome',  // You can choose `chrome`, `edge` or `firefox` in this capability
+        'browser_version': 'latest',  // We support v83 and above. You can choose `latest`, `latest-beta`, `latest-1`, `latest-2` and so on, in this capability
         'os': 'os x',
         'os_version': 'big sur',
         'build': 'puppeteer-build-1',
-        'name': 'My first Puppeteer test',
+        'name': 'My first Puppeteer test',  // The name of your test and build. See browserstack.com/docs/automate/puppeteer/organize tests for more details
         'browserstack.username': process.env.BROWSERSTACK_USERNAME || 'YOUR_USERNAME',
         'browserstack.accessKey': process.env.BROWSERSTACK_ACCESS_KEY || 'YOUR_ACCESS_KEY'
     };
     const browser = await puppeteer.connect({
     browserWSEndpoint:
-    `wss://cdp.browserstack.com/puppeteer?caps=${encodeURIComponent(JSON.stringify(caps))}`,
+    `wss://cdp.browserstack.com/puppeteer?caps=${encodeURIComponent(JSON.stringify(caps))}`,  // The BrowserStack CDP endpoint gives you a `browser` instance based on the `caps` that you specified
     });
 
     /* 
-    *  BrowserStack specific code ends here
+    *  The BrowserStack specific code ends here. Following this line is your test script.
+    *  Here, we have a simple script that opens google.com, searches for the word BrowserStack and asserts the result.
     */
     const page = await browser.newPage();
     await page.goto('https://www.duckduckgo.com');
@@ -36,5 +37,5 @@ const expect = require('chai').expect;
     } catch {
         await page.evaluate(_ => {}, `browserstack_executor: ${JSON.stringify({action: 'setSessionStatus',arguments: {status: 'failed',reason: 'Title did not match'}})}`);
     }
-    await browser.close();
+    await browser.close();  // At the end of each of your tests, you must close the browser so that BrowserStack knows when to end the session.
 })();
